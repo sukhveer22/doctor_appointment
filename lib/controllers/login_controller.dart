c057da9ea94d4e672bd457edc6bcaa4eb679ae9b
gurpyar <gurpyarsingh@technologies.com>
2024-08-09 13:22:31 +0530

    save data hon login

--- lib/controllers/login_controller.dart
+++ lib/controllers/login_controller.dart
@@ -1,3 +1,5 @@
+import 'package:doctor_appointment/controllers/sign_controller.dart';
+import 'package:doctor_appointment/models/user_model.dart';
 import 'package:doctor_appointment/screens/dashborad/dashboard.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -21,12 +23,21 @@ class LoginController extends GetxController {
         Get.snackbar('Error', 'Password cannot be empty');
       } else {
         isLoading.value = true; // Start loading
-        await FirebaseAuth.instance.signInWithEmailAndPassword(
+        UserCredential userData =
+            await FirebaseAuth.instance.signInWithEmailAndPassword(
           email: emailController.text.trim(),
           password: passwordController.text.trim(),
         );
-        Get.to(DashboardScreen());
-        Get.snackbar('Success', 'Logged in successfully');
+        if (userData.user != null) {
+          saveUserToPreferences(UserModel(
+            id:userData.user?.uid??"",
+            name: userData.user?.displayName??"",
+            email: userData.user?.email??"",
+            profilePicture:userData.user?.photoURL??"",
+          ));
+          Get.to(DashboardScreen());
+          Get.snackbar('Success', 'Logged in successfully');
+        }
       }
     } catch (e) {
       Get.snackbar('Error', e.toString());
